e95482e0dc3b5a3bb3ea4c7ac33ff881f468e3aa
Marcelus <mebsport@gmail.com>
2022-01-31 09:12:49 -0500

    added button functionality

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-
+//click on socials
+//get buttons
+const githubButton = document.getElementById('github-link');
+const youtubeButton = document.getElementById('youtube-link');
+const instagramButton = document.getElementById('instagram-link');
+console.log(githubButton);
+console.log(youtubeButton);
+console.log(instagramButton);
+//on click methods
+githubButton.addEventListener('click', () => {
+  console.log('Github Button Pressed');
+  window.open('https://github.com/mebsport', '_blank');
+});
+youtubeButton.addEventListener('click', () => {
+  console.log('YouTube Button Pressed');
+  window.open(
+    'https://www.youtube.com/channel/UCAfLIonq6STwupVN8KY40oA',
+    '_blank'
+  );
+});
+instagramButton.addEventListener('click', () => {
+  console.log('Instagram Button Pressed');
+  window.open('https://www.instagram.com/mebsport/', '_blank');
+});
