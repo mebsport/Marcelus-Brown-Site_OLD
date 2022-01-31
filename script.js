38171ceb9491aaf18efc5dc985c878de4d1b1057
Marcelus <mebsport@gmail.com>
2022-01-31 09:16:44 -0500

    extra stuff with buttons etc.

--- script.js
+++ script.js
@@ -3,22 +3,22 @@
 const githubButton = document.getElementById('github-link');
 const youtubeButton = document.getElementById('youtube-link');
 const instagramButton = document.getElementById('instagram-link');
-console.log(githubButton);
-console.log(youtubeButton);
-console.log(instagramButton);
+// console.log(githubButton);
+// console.log(youtubeButton);
+// console.log(instagramButton);
 //on click methods
 githubButton.addEventListener('click', () => {
-  console.log('Github Button Pressed');
+  console.log('Open GitHub');
   window.open('https://github.com/mebsport', '_blank');
 });
 youtubeButton.addEventListener('click', () => {
-  console.log('YouTube Button Pressed');
+  console.log('Open YouTube');
   window.open(
     'https://www.youtube.com/channel/UCAfLIonq6STwupVN8KY40oA',
     '_blank'
   );
 });
 instagramButton.addEventListener('click', () => {
-  console.log('Instagram Button Pressed');
+  console.log('Open Instagram');
   window.open('https://www.instagram.com/mebsport/', '_blank');
 });
